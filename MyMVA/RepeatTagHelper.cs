8be1ce8806f4d5bf6ee14b628324a98716c5cf21
Borys Romanyuk <borys.romanyuk@solera.com>
2018-06-26 14:09:00 -0500

    Added repeat helper tag

--- MyMVA/RepeatTagHelper.cs
+++ MyMVA/RepeatTagHelper.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MyMVA
 {
-    public class RepeatTagHelper
+    public class RepeatTagHelper : TagHelper
     {
+        public int Count { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                output.Content.AppendHtml(await output.GetChildContentAsync(useCachedResult: false));
+            }
+        }
     }
 }
